659c9fab830a4954a183ec57ac87189d574919a5
ryanjohnson0430 <ryanjohnson0430@gmail.com>
2025-07-09 10:56:31 -0400

    removing cname from main

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return (
     <section>
-      <h2 className="text-3xl font-semibold mb-4">Contact page</h2>
+      <h2 className="text-3xl font-semibold mb-4">Contact us</h2>
     </section>
   );
 }
